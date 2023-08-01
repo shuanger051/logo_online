b9ff553416d30caa3b87c9cb74e90e07d98d56df
yangyu <yangyu@fxsl.wecom.work>
2023-08-01 14:40:28 +0800

    moidfy

--- WEB/packages/lower-code/src/components/core/plugins/lbp-picture.js
+++ WEB/packages/lower-code/src/components/core/plugins/lbp-picture.js
@@ -23,7 +23,7 @@ export default {
       );
     } else {
       style.flexDirection = "column";
-      style.return(
+      return(
         <div style={style}>
           <img src={url} width="100%" />
         </div>
