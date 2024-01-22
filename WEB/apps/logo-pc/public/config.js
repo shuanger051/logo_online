03e10c86fee3698ba47abb66c0ff0f5bc514a753
JiuL <lggtg@qq.com>
2024-01-22 16:56:01 +0800

    配置修改

--- WEB/apps/logo-pc/public/config.js
+++ WEB/apps/logo-pc/public/config.js
@@ -1,2 +1,2 @@
-// window.__baseUrl = "https://gzt.cgw.hangzhou.gov.cn/txcg/cdsdz/"; //'http://47.96.150.44:8090/'
-window.__baseUrl = "http://47.96.150.44:8090/";
+window.__baseUrl = "https://gzt.cgw.hangzhou.gov.cn/txcg/cdsdz_oss/"; //'http://47.96.150.44:8090/'
+// window.__baseUrl = "http://47.96.150.44:8090/";
