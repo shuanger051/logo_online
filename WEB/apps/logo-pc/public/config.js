bd01704116caba73c0698df5e6ea224a7ebabeab
JiuL <lggtg@qq.com>
2024-01-24 19:49:31 +0800

    新增跳转页

--- WEB/apps/logo-pc/public/config.js
+++ WEB/apps/logo-pc/public/config.js
@@ -1,3 +1,4 @@
 window.__baseUrl = "https://gzt.cgw.hangzhou.gov.cn/txcg/cdsdz_oss/";
 // window.__baseUrl = "http://47.96.150.44:8090/";
 window.__h5Url = "https://47.96.150.44/logo-h5/"; //h5地址
+window.__pcUrl = "https://47.96.150.44"
